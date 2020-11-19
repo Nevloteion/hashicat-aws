08df2d04a44d4180ee1a4288bd94f0ea94e15a61
Nevloteion <loic.caufriez@gmail.com>
2020-11-19 11:33:19 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/caufriez-training/s3-bucket/aws"
   version = "1.16.0"
   # insert required variables here
-  bucket_prefix = "${PREFIX}"
+  bucket_prefix = vat.prefix
 }
